c837b56fbc6d66a4430f93c26180728a4d55c5b8
Flavio Ricardo <fhricardo@gmail.com>
2024-09-25 15:01:55 -0300

    Update IF/ELSE

--- ifelse.php
+++ ifelse.php
@@ -1,8 +1,16 @@
 <?php
 include 'includes/header.php';
+
 echo "<h2>Exemplo de utilização de IF / ELSE</h2>";
+
 echo '<a href="index.php"><button>Voltar</button></a>';
+
 $idade = $_POST['idade'];
+//if(condição){
+//      trecho de código para executar, caso a condição seja atendida.
+//}else{
+//      trecho de código para executar, caso nenhuma condição seja atendida.
+//}
 if ($idade < 18) {
     echo "Com {$idade} anos, você é considerado menor de idade!";
 } else if ($idade < 60) {
