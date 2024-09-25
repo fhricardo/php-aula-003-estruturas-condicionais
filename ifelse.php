d5cdbc781778e70d99d2a2735a8e614af67e5d99
Flavio Ricardo <fhricardo@gmail.com>
2024-09-25 14:43:33 -0300

    Update 002

--- ifelse.php
+++ ifelse.php
@@ -1,6 +1,6 @@
 <?php
 include 'includes/header.php';
-echo "<h2>Exemplo de utilizaçã de IF / ELSE</h2>";
+echo "<h2>Exemplo de utilização de IF / ELSE</h2>";
 echo '<a href="index.php"><button>Voltar</button></a>';
 $idade = $_POST['idade'];
 if ($idade < 18) {
